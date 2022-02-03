209d2b8b97dd8fb460db6c47fc039ed7db2268e2
Maksim Rozhkov <mahagon.here@gmail.com>
2022-02-03 19:41:28 +0700

    Edited 'sumSalary'

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   let result = 0;
 
   for (let v of Object.values(salaries)) {
-    if (!isNaN(v) && isFinite(v) && typeof v === "number") {
+    if (!Number.isNaN(v) && Number.isFinite(v) && typeof v === "number") {
       result += v;
     }
   }
