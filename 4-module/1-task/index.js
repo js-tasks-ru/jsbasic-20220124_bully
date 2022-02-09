8abbcd0860f32fdddfffeb8af6899ad7af76ca19
Maksim Rozhkov <mahagon.here@gmail.com>
2022-02-09 19:26:07 +0700

    Created function 'makeFriendsList'

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const friendsUl = document.createElement("ul");
+
+  for (let i = 0; i < friends.length; i++) {
+    let liItem = document.createElement('li');
+    liItem.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    friendsUl.appendChild(liItem);
+  }
+
+  return friendsUl;
 }
