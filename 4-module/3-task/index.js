9257aad4912e33dfe62df80cc322f04c3fe6f484
Maksim Rozhkov <mahagon.here@gmail.com>
2022-02-10 18:57:50 +0700

    Created function 'highlight'

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  let row = Array.from(table.getElementsByTagName('tr'));
+
+  row.forEach(tr => {
+    let status = tr.lastElementChild.getAttribute("data-available");
+
+    if (status) {
+      status === "true" ? tr.classList.add("available") : tr.classList.add("unavailable");
+    } else {
+      tr.hidden = true;
+    }
+
+    if (!(tr.cells[2] === "Gender")) {
+      tr.cells[2].innerHTML === "m" ? tr.classList.add("male") : tr.classList.add("female");
+    }
+
+    if (tr.cells[1].innerHTML < 18) {
+      tr.style.textDecoration = "line-through";
+    }
+  });
 }
