dd05794a101b4e0120a29e62a7c001cacf141999
Maksim Rozhkov <mahagon.here@gmail.com>
2022-02-19 19:58:15 +0700

    Created class 'ProductCard'

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,45 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.elem = this.render();
+  }
+
+  render() {
+    let price = this.product.price.toFixed(2);
+    let productName = this.product.name;
+    let productImage = this.product.image;
+    let card;
+
+    card = createElement(`
+    <div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${productImage}" class="card__image" alt="product">
+        <span class="card__price">€${price}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${productName}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+    `);
+
+    card.addEventListener('click', (event) => {
+      let addButton = event.target.closest('button');
+
+      if (addButton) {
+        let addProductEvent = new CustomEvent('product-add', {
+          detail: this.product.id,
+          bubbles: true,
+        });
+
+        event.target.closest('.card').dispatchEvent(addProductEvent);
+      }
+    });
+
+    return card;
   }
 }
