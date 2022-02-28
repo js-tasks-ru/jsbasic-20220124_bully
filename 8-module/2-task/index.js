a59fbf3165d2ef4ee10b82d2403ff6b7dff723be
1option <mahagon.here@gmail.com>
2022-02-28 22:44:25 +0700

    1 & 2 task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,60 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+      </div>
+    </div>`);
+
+    let gridInner = this.elem.querySelector('.products-grid__inner');
+
+    for (let product of this.products) {
+      let card = new ProductCard(product);
+      card.elem.dataset.category = product.category;
+      card.elem.dataset.maxSpiciness = product.spiciness;
+
+      if (product.nuts) {
+        card.elem.dataset.noNuts = product.nuts;
+      }
+
+      if (product.vegeterian) {
+        card.elem.dataset.vegeterianOnly = product.vegeterian;
+      }
+
+      gridInner.insertAdjacentHTML('beforeend', card.elem.outerHTML);
+    }
+
+
+    return this.elem;
+  }
+
+  updateFilter(filters) {
+    this.filters = Object.assign(this.filters, filters);
+
+    let productsGridInner = this.elem.querySelector('.products-grid__inner');
+    productsGridInner.innerHTML = '';
+
+    for (let product of this.products) {
+      if (this.filters.noNuts === true && product.nuts === true) {
+        continue;
+      }
+      if (this.filters.vegeterianOnly === true && product.vegeterian !== true) {
+        continue;
+      }
+      if (this.filters.maxSpiciness !== undefined && this.filters.maxSpiciness < product.spiciness) {
+        continue;
+      }
+      if (this.filters.category !== undefined && this.filters.category != product.category) {
+        continue;
+      }
+      let productCard = new ProductCard(product);
+      productsGridInner.append(productCard.elem);
+    }
+
   }
 }
